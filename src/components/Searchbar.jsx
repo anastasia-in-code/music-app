29426fc8076b6842e57a8c2f5e3bd4eddd17efc2
Anastasiia <anastasiia.perih@gmail.com>
2023-12-03 19:29:04 -0500

    Search component UI

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,5 +1,23 @@
-const Searchbar = () => (
-  <div>Loader</div>
-);
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { FiSearch } from 'react-icons/fi';
+
+const Searchbar = () => {
+  const [searchVal, setSearchVal] = useState('');
+  const handleChange = (e) => {
+    setSearchVal(e.target.value);
+  };
+  return (
+    <form autoComplete="off" className="p-2 text-gray-400 focus-within:text-gray-600">
+      <label htmlFor="search-field" className="sr-only">
+        Search all songs
+      </label>
+      <div className="flex flex-row justify-start items-center ">
+        <FiSearch className="w-5 h-5 ml-4" />
+        <input className="flex-1 bg-transparent border-none outline-none placeholder-gray-500 text-base text-white" name="search-field" autoComplete="off" id="search-field" placeholder="Search" type="search" value={searchVal} onChange={handleChange} />
+      </div>
+    </form>
+  );
+};
 
 export default Searchbar;
